9f02a928f96dcc7810bbcfed155a81d6b1b2d48b
santidelosrios <sdelosrios95@gmail.com>
2015-11-25 04:15:06 -0500

    script.js

---
+++ public/script.js
@@ -0,0 +1,39 @@
+var thedata = "";
+var selectFile; 
+var reader;
+var sending = function sendFile(){
+	selectFile = $("#file_upload")[0].files[0];
+	reader = new FileReader();
+	reader.onload = function(event) {
+	    var contents = event.target.result;
+	    console.log("File contents: " + contents);
+	};
+	reader.onerror = function(event) {
+	    console.error("File could not be read! Code " + event.target.error.code);
+	};
+
+	reader.readAsText(selectFile);
+ }
+function sendpost(){
+	console.log(reader.result);
+	thedata = reader.result; 
+	var sendInfo  = {
+           data: thedata
+       };
+	var data = sending();
+	     $.ajax({
+           type: "POST",
+           url: "/sendFile",
+           dataType: "json",
+           data: sendInfo,
+           success: function (msg) {
+               if (msg) {
+                   console.log("Somebody" + name + " was added in list !");
+                   //location.reload(true);
+               } else {
+                   alert("Cannot add to list !");
+               }
+           }
+       });
+	console.log(thedata);
+}
